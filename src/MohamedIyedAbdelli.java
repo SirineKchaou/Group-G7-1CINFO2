05ad949f033b535dcb8be85d029546b7b3ac8d75
iyedAbd <mohamediyed.abdelli@esprit.tn>
2025-02-01 15:19:52 +0100

    1er depot

---
+++ src/MohamedIyedAbdelli.java
@@ -0,0 +1,10 @@
+public class MohamedIyedAbdelli {
+    int table[] ;
+    public int sommeTableau(){
+        int s=0;
+        for(int i=0;i<table.length;i++){
+            s=s+table[i];
+        }
+        return s;
+    }
+}
