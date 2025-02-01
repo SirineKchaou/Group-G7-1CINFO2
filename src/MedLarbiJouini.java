46aebc81a93d4cc07585566ec67c3d21887d7562
Mohamed Larbi Jouini <MohamedLarbi.JOUINI@Esprit.tn>
2025-02-01 15:11:22 +0100

    Calculer la racine carrée d’un nombre

---
+++ src/MedLarbiJouini.java
@@ -0,0 +1,17 @@
+public class MedLarbiJouini {
+    // Calculer la racine carrée d’un nombre
+    public static double racineCarree(double n) {
+        return Math.sqrt(n);
+    }
+
+    // Convertir une chaîne en majuscule
+    public static String enMajuscule(String s) {
+        return s.toUpperCase();
+    }
+
+    // Calculer la longueur d’une chaîne
+    public static int longueurChaine(String s) {
+        return s.length();
+    }
+
+}
